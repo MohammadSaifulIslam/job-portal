db6a1de6f6a8686f53b76ce59ede375939564aa7
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-05-16 23:01:21 -0700

    seacr jobs by title and category implement in all jobs component

--- src/pages/AllJobsPage/AllJobsPageHome.jsx
+++ src/pages/AllJobsPage/AllJobsPageHome.jsx
@@ -5,26 +5,38 @@ import JobCard from "../HomaPage/AvailableJobs/JobCard/JobCard";
 const AllJobsPageHome = () => {
     const [jobs, setJobs] = useState([]);
 
+    // seacrh jobs by title and category
+    const [searchText, setSearchText] = useState('');
+    const handleSearchJobs = () => {
+        console.log({ searchText });
+        fetch(`http://localhost:5000/getJobsBySearch/${searchText}`)
+            .then(res => res.json())
+            .then(data => {
+                setJobs(data)
+            })
 
+
+    }
     useEffect(() => {
         fetch(`http://localhost:5000/allJobs/text`)
             .then(res => res.json())
             .then(data => {
-                console.log({ data })
                 setJobs(data)
             })
     }, [])
+
     return (
         <div className="my-container my-20">
             <div className="section-title text-center mb-10">
-                    <h2 className="text-3xl font-bold mb-2">Search and Find Job</h2>
-                    <p>Most popular categories of portal, sorted by popularity</p>
-                </div>
-             <div className='flex gap-4 mt-5 p-5 rounded-xl shadow-xl bg-[#f9fcff] '>
-                        <input type="text" placeholder='Job Title' className='my-input' />
-                        <input type="text" placeholder='Job Category' className='my-input' />
-                        <button><MagnifyingGlassCircleIcon className='h-12 w-12 text-red-500'></MagnifyingGlassCircleIcon></button>
-                    </div>
+                <h2 className="text-3xl font-bold mb-2">Search and Find Job</h2>
+                <p>Most popular categories of portal, sorted by popularity</p>
+            </div>
+            <div className='flex gap-4 mt-5 p-5 items-center rounded-xl shadow-xl bg-[#f9fcff] '>
+                <input onChange={(e) => setSearchText(e.target.value)} type="text" placeholder='Job Title or Job Category' className='my-input' />
+                {/* <span className="text-red-500 text-lg">or</span>
+                <input onChange={(e) => setSearchText(e.target.value)} type="text" placeholder='Job Category' className='my-input' /> */}
+                <button onClick={handleSearchJobs}><MagnifyingGlassCircleIcon className='h-12 w-12 text-red-500'></MagnifyingGlassCircleIcon></button>
+            </div>
             <div className="mt-20 grid md:grid-cols-2 lg:grid-cols-2 gap-5">
                 {
                     jobs.map(job => <JobCard
--- src/pages/HomaPage/AvailableJobs/AvailableJobs.jsx
+++ src/pages/HomaPage/AvailableJobs/AvailableJobs.jsx
@@ -11,7 +11,6 @@ const AvailableJobs = () => {
         fetch(`http://localhost:5000/allJobs/${status}`)
             .then(res => res.json())
             .then(data => {
-                console.log({ data })
                 setJobs(data)
             })
     }, [status])
